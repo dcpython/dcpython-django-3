992341a6c94c93e6f6bb408aee51f26d2896efdd
Alex Clark <aclark@aclark.net>
2018-03-17 08:23:16 -0400

    Update

--- dcpython/urls.py
+++ dcpython/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^donate$', views.donate, name='donate'),
-    url(r'^jobs$', views.donate, name='jobs'),
+    url(r'^jobs$', views.jobs, name='jobs'),
     url(r'^legal$', views.legal, name='legal'),
     url(r'^andrew-w-singer$', views.aws, name='aws'),
     url(r'^code-of-conduct$', views.coc, name='coc'),
